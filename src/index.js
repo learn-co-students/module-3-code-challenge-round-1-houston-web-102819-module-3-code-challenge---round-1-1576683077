ade04234838f5734b4e02d702e03737697b34320
github username <github email address>
2019-12-18 11:15:37 -0600

    hello

--- src/index.js
+++ src/index.js
@@ -1,12 +1,70 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 4205
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
-  const likeURL = `https://randopic.herokuapp.com/likes/`
+  const likeURL = `https://randopic.herokuapp.com/likes`
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
+  let imageCard = document.querySelector("image_card")
+
+  fetch(imageURL)
+    .then(function (response) {
+      return response.json()
+    }).then(function (id) {
+      console.log(id.comments)
+      let imageSelect = document.getElementById("image")
+      imageSelect.setAttribute('src', id.url)
+
+      let imageName = document.getElementById("name")
+      imageName.innerHTML = id.name
+
+      let numberOfLikes = document.getElementById("likes")
+      numberOfLikes.innerHTML = id.like_count
+
+
+
+      let imageComments = document.getElementById("comments")
+      id.comments.forEach(function (comment) {
+        let firstComment = comment.content
+        imageComments.innerHTML = firstComment
+      })
+      button = document.getElementById("like_button")
+      button.addEventListener('click',function(){
+      likeButton = document.getElementById("likes")
+      likeButton.innerHTML++
+      fetch(likeURL, {
+        method: 'POST',
+        body: JSON.stringify({
+          image_id: 4205
+        }),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      })
+      commentsUL = document.getElementById("comments")
+      createLi = document.createElement("li")
+      createLi.forEach(function(comments)
+
+
+
+      })
+
+
+      
+
+
+
+      
+
+
+      
+
+
+    })
+
 
 })
